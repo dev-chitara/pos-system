2efc716423260979ad5e96cd962f6a9d1953ade8
Dev Chitara <devchitara2003@gmail.com>
2023-10-15 18:06:27 +0530

    Completed Customer Functionality

---
+++ main.py
@@ -0,0 +1,18 @@
+from schemas.customers import get_customers, create_customers, get_customer, update_customer, delete_customer
+
+# create_customers("Harshit", 9882686578)
+# create_customers("Harsh", 7612576893)
+# create_customers("Mohit", 9774214672)
+# create_customers("Rahul", 7834791814)
+# create_customers("Dev", 8941626486)
+# create_customers("Sanjay", 1298126825)
+
+
+# print(get_customers())
+
+# print(get_customer("ae90a69c-db75-461b-8bd4-f3130bfe83ce"))
+
+# print(update_customer("ae90a69c-db75-461b-8bd4-f3130bfe83ce", "Vinod", 8635268631))
+
+# print(delete_customer("1827c35e-6581-48e7-b4f7-ca821e1689b5"))
+
--- schemas/customers.py
+++ schemas/customers.py
@@ -1,5 +1,9 @@
+import csv
 import uuid
-import json
+
+
+PATH = "/home/dev/workspace/projects/pos-system/db/customers.csv"
+FIELDNAMES = ["id", "name", "contact_number"]
 
 class Customer:
     def __init__(self, name, contact_number):
@@ -30,9 +34,85 @@ class Customer:
         
         self.name = kwargs.get("name", self.name)
         self.contact_number = kwargs.get("contact_number", self.contact_number)
-        
+
 
     def get_customer_details(self):
         return self.__dict__
 
 
+def get_customers():
+    with open(PATH, "r") as file:
+        reader = csv.DictReader(file)
+
+        records = []
+        for item in reader:
+            records.append(item)
+        return records
+
+
+def create_customers(name, contact_number):
+    customer = Customer(name, contact_number)
+    customer_dict = customer.get_customer_details()
+
+    with open(PATH, "a") as file:
+        writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
+        writer.writerow(customer_dict)
+
+
+def get_customer(id):
+    with open(PATH, "r") as file:
+        reader = csv.DictReader(file)
+
+        for item in reader:
+            if item["id"] == id:
+                return item
+
+
+def update_customer(id, name, contact_number):
+   with open(PATH, "r") as file:
+        reader = csv.DictReader(file)
+
+        records = []
+        is_exists = False
+
+        for item in reader:
+            if item["id"] == id:
+                item["name"] = name
+                item["contact_number"] = contact_number
+                is_exists = True
+            records.append(item)
+
+        if is_exists:
+            with open(PATH, "w") as file:
+                writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
+                writer.writeheader()
+                writer.writerows(records)
+                
+        else:
+            return (is_exists, "Customer does not exists")
+        
+        return (is_exists, "Successfully updated")
+
+
+def delete_customer(id):
+   with open(PATH, "r") as file:
+        reader = csv.DictReader(file)
+
+        records = []
+        is_exists = False
+
+        for item in reader:
+            if item["id"] != id:
+                is_exists = True
+                records.append(item)
+
+        if is_exists:
+            with open(PATH, "w") as file:
+                writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
+                writer.writeheader()
+                writer.writerows(records)
+                
+        else:
+            return (is_exists, "Customer does not exists")
+        
+        return (is_exists, "Successfully deleted")
