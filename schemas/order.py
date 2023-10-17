706a8d24f6b50169decfff024b255cf69726da83
Dev Chitara <devchitara2003@gmail.com>
2023-10-17 20:46:55 +0530

    Some changes

--- schemas/order.py
+++ schemas/order.py
@@ -7,13 +7,21 @@ FIELDNAMES = []
 
 
 class Order:
-    def __init__(self, customer_id, sold_product, payment): 
+    def __init__(self, customer_id, sold_product, payment_type = "cash"): 
         self.id = uuid.uuid4()
         self.date = datetime.now()
         self.customer_id = customer_id
         self.sold_product = sold_product
         self.total_order_amount = 0
-        self.payment = payment
+        self.payment_type = payment_type
+
+    def set_sold_product(self, product_id, sold_quanity):
+        self.sold_product = {}
+        self.sold_product[f"{product_id}"] = sold_quanity
+
+
+
+
 
 
 
