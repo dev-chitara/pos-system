0d77b1011c81389fba89baae8c692bc0109ea777
Dev Chitara <devchitara2003@gmail.com>
2023-10-17 22:10:12 +0530

    completed order functionality

--- schemas/order.py
+++ schemas/order.py
@@ -8,25 +8,27 @@ FIELDNAMES = []
 
 
 class Order:
-    def __init__(self, customer_id, sold_product, payment_type): 
+    def __init__(self, customer_id, product_id, payment_type): 
         self.id = uuid.uuid4()
         self.date = datetime.now()
         self.customer_id = customer_id
-        self.sold_product = sold_product
+        self.product_id = product_id
+        self.sold_product = {}
         self.total_order_amount = 0
         if type(payment_type) == None:
             self.payment_type = "cash"
         else:
             self.payment_type = payment_type
 
-    def set_sold_product(self, product_id, sold_quanity):
-        self.sold_product = {}
-        self.sold_product["product_id"] = product_id
+    def set_sold_product(self,sold_quanity):
+        self.sold_product["product_id"] = self.product_id
         self.sold_product["sold_quanity"] = sold_quanity
-        product_dict = get_product(self.sold_product.get(product_id))
+        product_dict = get_product(self.sold_product.get(self.product_id))
         self.sold_product["total_amount"] = product_dict["price"]*self.sold_product.get(sold_quanity)
     
-    
+    def set_total_order_amonut(self):
+        self.total_order_amount  = self.sold_product["total_amount"]
+
 
 
 
